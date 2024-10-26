8efcec36ff9e5398744d96eecab980a3dc0c2171
Danny-jin <dannyjin0215@gmail.com>
2024-10-26 16:13:58 -0700

    updated

---
+++ maps.py
@@ -0,0 +1,12 @@
+from pprint import pprint
+import googlemaps
+
+API_KEY = 'AIzaSyCNgZ4vqkNiSw5jTDRzam1dAFqjFgkVWRs'
+
+map_client = googlemaps.Client(API_KEY)
+
+work_place_address = "9450 Gilman Drive, La Jolla, CA"
+
+response = map_client.geocode(work_place_address)
+
+pprint(response)
